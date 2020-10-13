34da4bba3a05dfa4ba8a531f62f311d3f6073dce
Aureliano <aurelianosza@gmail.com>
2020-10-13 19:35:15 -0300

    change system folder to root

--- system/system_log.py
+++ system/system_log.py
@@ -2,7 +2,7 @@ import path
 import sys
 
 folder = path.Path(__file__).abspath()
-sys.path.append(folder.parent.parent.parent)
+sys.path.append(folder.parent.parent)
 
 from interfaces.observer import Observer
 
