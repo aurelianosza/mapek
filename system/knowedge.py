33ec7a35817ac402f4c82357e63dfeaa1e6d9bd6
Aureliano <aurelianosza@gmail.com>
2020-11-26 14:47:28 -0300

    adding system knowedge

--- system/knowedge.py
+++ system/knowedge.py
@@ -7,14 +7,47 @@ sys.path.append(folder.parent.parent)
 class Knowedge(object):
 
     def __init__(self):
-        self.create = None
-        self.read = None
-        self.update = None
-        self.delete = None
+        self.creator = None
+        self.reader = None
+        self.updater = None
+        self.deletor = None
 
     def create(self, data):
-        if self.create is None
+        if self.create is None:
             return
-        self.create(data)
+        self.creator.execute(data)
 
+    def read(self, data):
+        if data is None:
+            return None
+        return self.reader.execute(data)
+
+    def update(self, data):
+        if data is None:
+            return None
+        self.updater.execute(data)
+
+    def delete(self, data):
+        if data is None:
+            return None
+        self.deletor.execute(data)
+
+
+if __name__ == '__main__':
+    
+    from system.strategies.knowedge_creator_strategy import KnowedgeCreatorStrategy
+    from system.strategies.knowedge_deletor_strategy import KnowedgeDeletorStrategy
+    from system.strategies.knowedge_reader_strategy import KnowedgeReaderStrategy
+
+    k = Knowedge()
+
+    k.creator = KnowedgeCreatorStrategy()
+    k.deletor = KnowedgeDeletorStrategy()
+    k.reader = KnowedgeReaderStrategy()
+
+    k.create({"flow": 'podcast'})
+    print('{}'.format(k.read('flow')))
+    k.delete('flow')
+
+    
      
